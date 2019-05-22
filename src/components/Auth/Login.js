b63e3aa82a939bf6ba1f61a6563896493c2794c3
Harry Gothold <harrygot@hotmail.com>
2019-05-22 22:56:38 +0100

    added reusable hook

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+
+import useFormValidation from "./useFormValidation";
+
+const INITIAL_STATE = {
+  name: "",
+  email: "",
+  password: ""
+};
 
 function Login(props) {
-  return <div>Login</div>;
+  const { handleChange, handleSubmit, values } = useFormValidation(
+    INITIAL_STATE
+  );
+  const [login, setLogin] = useState(true);
+  return (
+    <div>
+      <h2 className="mv3">{login ? "Login" : "Create Account"}</h2>
+      <form onSubmit={handleSubmit} className="flex flex-column">
+        {!login && (
+          <input
+            type="text"
+            placeholder="Your name"
+            autoComplete="off"
+            value={values.name}
+            onChange={handleChange}
+            name="name"
+          />
+        )}
+        <input
+          type="email"
+          placeholder="Your email"
+          autoComplete="off"
+          onChange={handleChange}
+          name="email"
+          value={values.email}
+        />
+        <input
+          type="password"
+          placeholder="Choose a secure password"
+          onChange={handleChange}
+          name="password"
+          value={values.password}
+        />
+        <div className="flex mt3">
+          <button type="submit" className="button pointer mr2">
+            Submit
+          </button>
+          <button
+            onClick={() => setLogin(prevLogin => !prevLogin)}
+            type="button"
+            className="pointer button"
+          >
+            {login ? "Need to create an account?" : "Already have an account?"}
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
--- src/components/Auth/useFormValidation.js
+++ src/components/Auth/useFormValidation.js
@@ -1,5 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
-function useFormValidation() {}
+function useFormValidation(initialState) {
+  const [values, setValues] = useState(initialState);
+  function handleChange(e) {
+    e.persist();
+    setValues(previousValues => ({
+      ...previousValues,
+      [e.target.name]: e.target.value
+    }));
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log({ values });
+  }
+  return { handleChange, handleSubmit, values };
+}
 
 export default useFormValidation;
