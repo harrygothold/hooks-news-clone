bcc8933bcd9df2efa884ccd463ef0a86942a2846
Harry Gothold <harrygot@hotmail.com>
2019-09-22 13:30:46 +0100

    some style changes and added a firebase offset pagination function

--- src/components/Link/LinkList.js
+++ src/components/Link/LinkList.js
@@ -2,14 +2,17 @@ import React, { useEffect, useState, useContext } from "react";
 import FirebaseContext from "../firebase/context";
 import LinkItem from "./LinkItem";
 import { LINKS_PER_PAGE } from "../../utils";
+import axios from "axios";
 
 const LinkList = props => {
   const { firebase } = useContext(FirebaseContext);
   const [links, setLinks] = useState([]);
   const [cursor, setCursor] = useState(null);
+  const [loading, setLoading] = useState(false);
   const isNewPage = props.location.pathname.includes("new");
   const isTopPage = props.location.pathname.includes("top");
   const page = Number(props.match.params.page);
+  const linksRef = firebase.db.collection("links");
 
   useEffect(() => {
     const unsubscribe = getLinks();
@@ -18,25 +21,37 @@ const LinkList = props => {
 
   const getLinks = () => {
     const hasCursor = Boolean(cursor);
+    setLoading(true);
     if (isTopPage) {
-      return firebase.db
-        .collection("links")
+      return linksRef
         .orderBy("voteCount", "desc")
         .limit(LINKS_PER_PAGE)
         .onSnapshot(handleSnapshot);
     } else if (page === 1) {
-      return firebase.db
-        .collection("links")
+      return linksRef
         .orderBy("created", "desc")
         .limit(LINKS_PER_PAGE)
         .onSnapshot(handleSnapshot);
     } else if (hasCursor) {
-      return firebase.db
-        .collection("links")
+      return linksRef
         .orderBy("created", "desc")
         .startAfter(cursor.created)
         .limit(LINKS_PER_PAGE)
         .onSnapshot(handleSnapshot);
+    } else {
+      const offset = page * LINKS_PER_PAGE - LINKS_PER_PAGE;
+      axios
+        .get(
+          `https://us-central1-hooks-news-clone.cloudfunctions.net/linksPagination?offset=${offset}`
+        )
+        .then(res => {
+          const links = res.data();
+          const lastLink = links[links.length - 1];
+          setLinks(links);
+          setCursor(lastLink);
+          setLoading(false);
+        });
+      return () => {};
     }
   };
 
@@ -47,6 +62,7 @@ const LinkList = props => {
     const lastLink = links[links.length - 1];
     setLinks(links);
     setCursor(lastLink);
+    setLoading(false);
   };
 
   const visitPreviousPage = () => {
@@ -64,7 +80,7 @@ const LinkList = props => {
   const pageIndex = page ? (page - 1) * LINKS_PER_PAGE + 1 : 0;
 
   return (
-    <div>
+    <div style={{ opacity: loading ? 0.25 : 1 }}>
       {links.map((link, index) => (
         <LinkItem
           key={link.id}
